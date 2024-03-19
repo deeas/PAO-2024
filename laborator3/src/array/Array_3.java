07b2b769c090dc955397aa01b0a94ae27224154e
Andreea Ciocan <andreea.ciocan@endava.com>
2024-03-19 22:01:47 +0200

    Laborator 3

---
+++ laborator3/src/array/Array_3.java
@@ -0,0 +1,25 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Array_3 {
+    public static void main(String[] args) {
+        Integer[] ints = new Integer[10];
+
+        System.out.println("Initialise array: ");
+        Random random = new Random();
+
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = random.nextInt(10);
+            System.out.print(ints[i] + " ");
+        }
+
+        Arrays.sort(ints);
+        System.out.println("Sorted array: ");
+
+        for (int i = 0; i < ints.length; i++) {
+            System.out.print(ints[i] + " ");
+        }
+    }
+}
---
+++ laborator3/src/practice/Ex_1.java
@@ -0,0 +1,25 @@
+package practice;
+
+/**
+ * Write a Java program to remove the duplicates from a given array
+ * and return the new length of the array
+ */
+public class Ex_1 {
+    public static void main(String[] args) {
+        int[] array = {10, 7, 67, 67, 78, 34, 45, 34, 11, 12, 11, 34, 10};
+
+        int len = array.length;
+        int[] app = new int[101];
+
+        for (int value : array) {
+            if (app[value] == 0) {
+                System.out.print(value + " ");
+                app[value]++;
+
+            } else {
+                len--;
+            }
+        }
+        System.out.println("Length: " + len);
+    }
+}
